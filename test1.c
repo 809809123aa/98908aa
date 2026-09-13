a44117142d3b20b231681e1669cefbb87fc560c2
809809123aa <liu88777@126.com>
2026-09-13 14:55:56 +0800

    提交test1

---
+++ test1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	printf("11111222");
+	printf("11111222");
+	printf("11111222");
+
+
+
+	printf("11111222");
+
+
+
+	printf("11111222");
+	return 0;
+}
+
